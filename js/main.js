4d58ab79d29b3e14b684cbe01da90906e3a05cfa
Haider Prieto <haiderandrey@gmail.com>
2024-02-02 15:56:04 -0500

    Entrega final

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
-// Array de servicios a ofrecer
+const infoAlertBorrar = {
+    titulo: "¡Oh nooo!",
+    texto: "¿Seguro quieres borrar este servicio?... Igualmente, puedes agregar el servicio luego",
+    confirmarBoton: "Si, borrar",
+    cancelarBoton: "No, mantener",
+    tituloExitoso: "¡Perfecto!",
+    textoExitoso: "Se elminó el servicio correctamente",
+}
 
-const cards = [
+let cards = [];
 
-    {
-        id: 1,
-        titulo: "Valoración inicial",
-        precio: 200000,
-        descripcion: "Inicia tu tratamiento con la Dra. Clara Tabares realizando una valoración general y específica"
-    },
-    {
-        id: 2,
-        titulo: "Sueroterapia",
-        precio: 250000,
-        descripcion: "Inicia tu tratamiento con la Dra. Clara Tabares realizando una valoración general y específica"
-    },
-    {
-        id: 3,
-        titulo: "Teleconsulta",
-        precio: 200000,
-        descripcion: "Inicia tu tratamiento con la Dra. Clara Tabares realizando una valoración general y específica"
-    },
-    {
-        id: 4,
-        titulo: "Plasma",
-        precio: 500000,
-        descripcion: "Inicia tu tratamiento con la Dra. Clara Tabares realizando una valoración general y específica"
-    }
-
-];
-
-// Obtener div contenedor en el DOM
-
+// Obtener divs del DOM
 const contenedor = document.getElementById("contenedorCards");
-
+const modalCarrito = document.getElementById("modalCarrito");
+const contenedorCarrito = document.getElementById("contenedorCarrito");
 // Se trae el carrito del localStorage
-
 const carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
+let activoModalCarrito = false;
+
+const cargarDatosYCrearCards = async () => {
+    // Esperamos a que fetch obtenga la respuesta
+    const response = await fetch("../datos/data.json");
+    // Esperamos a que la respuesta se convierta a JSON
+    const data = await response.json();
+    // Copiamos los datos obtenidos a cards
+    cards = [...data];
+    // Llamamos a cargaDeCards después de obtener y procesar los datos
+    cargaDeCards();
+    totalCarrito();
+    cargaDeCarrito();
+};
 
 // creación de los cards de los servicios del carrito
 const cargaDeCards = () => {
@@ -74,8 +67,6 @@ const cargaDeCards = () => {
 
 };
 
-cargaDeCards();
-
 const agregarCarrito = idCard => {
 
     // Se agrega ID del card en el carrito
@@ -113,9 +104,6 @@ const borrarCarrito = idCard => {
     totalCarrito();
 };
 
-const modalCarrito = document.getElementById("modalCarrito");
-const contenedorCarrito = document.getElementById("contenedorCarrito");
-
 // Función para actualizar carrito
 
 const cargaDeCarrito = () => {
@@ -133,7 +121,7 @@ const cargaDeCarrito = () => {
         <h3>${card.titulo}</h3>
         <h4>$${card.precio}</h4>
         
-        <button type="button" class="btn btn-danger btn-sm btnServiciosCarrito" onclick = "borrarCarrito(${card.id})">Borrar</>
+        <button type="button" class="btn btn-success btn-sm btnServiciosCarrito" onclick = "abrirSweetAlert(infoAlertBorrar, ${card.id})">Borrar</>
         
         </div>
         `;
@@ -144,12 +132,12 @@ const cargaDeCarrito = () => {
 
 };
 
-cargaDeCarrito();
-
 // Función para mostrar y ocultar carrito
 
-const mostrarCarrito = evento => {
-    if (evento) {
+const mostrarCarrito = () => {
+    activoModalCarrito = !activoModalCarrito;
+
+    if (activoModalCarrito) {
         modalCarrito.classList.add("showModal");
     } else {
         modalCarrito.classList.remove("showModal");
@@ -178,4 +166,28 @@ const totalCarrito = () => {
     bulletHtml.innerText = `${totalServicios}`;
 };
 
-totalCarrito();
+const abrirSweetAlert = (infoAlert, cardId) => {
+    Swal.fire({
+        title: infoAlert.titulo,
+        text: infoAlert.texto,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: infoAlert.confirmarBoton,
+        cancelButtonText: infoAlert.cancelarBoton,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            borrarCarrito(cardId);
+
+            Swal.fire({
+                title: infoAlert.tituloExitoso,
+                text: infoAlert.textoExitoso,
+                icon: "success"
+            });
+        }
+    });
+};
+
+cargarDatosYCrearCards();
+cargaDeCards();
